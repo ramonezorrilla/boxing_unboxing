a60d07181e13e1f1a6419da79ea68fe9ca17027b
ramonezorrilla <ramoenzorrilla@gmail.com>
2024-08-02 15:33:24 -0400

    Add project files.

---
+++ boxing_unboxing/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace boxing_unboxing 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            boxing_unboxing();
+
+        }
+
+        static void boxing_unboxing() 
+        
+        {
+            int numero1 = 20;
+            object obj1 = numero1; //boxing
+            int resultado = numero1 + (int)obj1; //unboxing
+            Console.WriteLine($"La suma de los valores es igual a : {resultado}");
+
+
+        }
+
+
+    
+    }
+
+
+}
